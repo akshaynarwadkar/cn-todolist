5cc0557e648d7c08c9c5b6cebc894dd1b78f7e10
akshaynarwadkar <akshaynarwadkar1@gmail.com>
2023-02-19 21:10:41 +0530

    final

--- todo.js
+++ todo.js
@@ -1,76 +1,11 @@
 
 
 let tasks=[]
-const taskList = document.getElementById('list');
+const tasksList = document.getElementById('list');
 const addTaskInput = document.getElementById('add');
 const tasksCounter = document.getElementById('tasks-counter');
-const enterbtnEl=document.getElementById('enter-btn')
-const dueDateEl=document.getElementById('dueDate')
-
-
-
-
-
-enterbtnEl.onclick=()=>{
-    taskList.innerHTML=`` 
-    tasks.push({
-        title:addTaskInput.value,
-        dueDate:dueDateEl.value,
-        id:new Date().getTime()
-    })
-    
-    console.log(tasks)
-    
-    rendertask()
-}
-
-function myfunction(event){
-    
-    tasks=tasks.filter(task=>{
-        return task.id!== parseInt(event.target.id)
-    })
-    rendertask()
-    
-}
-
-function rendertask(){
-    console.log('wokring')
-    taskList.innerHTML=''
-    console.log(tasks)
-    tasks.forEach((item)=>{
-        taskList.innerHTML+=`<li>${item.title} ${item.dueDate}</li>
-        <button onclick="myfunction(event)" id=${item.id} type="button">Delete</button > `
-    })   
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// const enterbtnEl=document.getElementById('enter-btn')
+// const dueDateEl=document.getElementById('dueDate')
 
 
 
@@ -78,24 +13,127 @@ function rendertask(){
 
 // console.log('Working');
 
-// function renderList () {}
+async function fetchAPI(){
 
-// function markTaskAsComplete (taskId) {}
+    // fetch('https://jsonplaceholder.typicode.com/todos')
+    // .then(response=>response.json())
+    // .then(data=>{
+    //     tasks=data.slice(0,10)
+    //     renderList()
+    // }
+    //     )
 
-// function deleteTask (taskId) {
+    const response=await fetch('https://jsonplaceholder.typicode.com/todos')
+    const data=await  response.json()
+    tasks=data.slice(0,10)
+    renderList()
+}
 
-// }
-
-// function addTask (task) {
-//     // document.addEventListener('keyup',addtaskitem)
+function renderList () {
+ 
+    tasksList.innerHTML=''
     
-//     enterbtnEl.onclick=()=>{
-//         taskList.innerHTML+=`<li>${addTaskInput.value}</li>` 
-//     }
+    tasks.forEach(task=>{
+        const li=document.createElement('li')
 
-    
-// }
+        li.innerHTML+=`
+        
+          <input type="checkbox" id="${task.id}" ${task.completed ? 'checked':''} class="custom-checkbox">
+          <label for="${task.id}">${task.title}</label>
+          <img src="/delete.png" class="delete" data-id="${task.id}" />
+         
+        `
+        tasksList.append(li)
 
-// addTask()
+    })
+}
 
-// function showNotification(text) {}
+function toggleTask (taskId) {
+    const task=tasks.filter(task=>{
+        return task.id==taskId
+        
+    })
+    console.log(task[0])
+    if(task.length>0){
+        const currentTask=task[0]
+        currentTask.completed = !currentTask.completed
+        renderList()
+        showNotification('Task Toggled successfully')
+    }
+}
+
+function deleteTask (taskId) {
+
+    // console.log(taskId)
+    const newTask=tasks.filter(function(task){ 
+        
+        return task.id!=taskId 
+    }) 
+    console.log(newTask)
+    tasks=newTask;
+
+    renderList()
+    showNotification('Task deleted successfully')
+
+}
+
+
+function addTask (task) {
+    if(task){
+        tasks.push(task)
+        renderList()
+        showNotification('Task added successfully')
+        return
+    }
+    showNotification('task cannot be added')
+   
+
+
+ }
+
+
+function showNotification(text) {
+    alert(text)
+}
+
+function handleInputKeypress(e){
+    if(e.key==="Enter"){
+        const text=e.target.value
+       
+        if(!text){
+            showNotification('Task text cannot be empty')
+        }
+
+        const task={
+            title:text,
+            id:Date.now().toString(),
+            completed:false
+        }
+        e.target.value=''
+        addTask(task)
+        console.log(task)
+    }
+}
+
+function handleclickListener(e){
+    const target=e.target
+    console.log(target)
+    if(target.className=='delete'){
+        const taskId=target.dataset.id
+        deleteTask(taskId)
+        return
+    }else if(target.className=='custom-checkbox'){
+        const taskId=target.id
+        // console.log(task)
+        toggleTask(taskId)
+        return
+    }
+}
+
+function initializeApp(){
+    fetchAPI()
+    addTaskInput.addEventListener('keyup',handleInputKeypress)
+    document.addEventListener('click',handleclickListener)
+}
+
+initializeApp()
